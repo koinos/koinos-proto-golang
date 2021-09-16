5315c654fbdacb6644391a7fe996873092f83724
Nathaniel Caldwell <nbcwell@gmail.com>
2021-09-16 17:53:56 -0400

    Add an additional test protobuf object

--- koinos/canonical/test_map.pb.go
+++ koinos/canonical/test_map.pb.go
@@ -67,6 +67,53 @@ func (x *testMap) GetImTheMap() map[uint64][]byte {
 	return nil
 }
 
+type mapWrapper struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	NestedMap *testMap `protobuf:"bytes,1,opt,name=nested_map,json=nestedMap,proto3" json:"nested_map,omitempty"`
+}
+
+func (x *mapWrapper) Reset() {
+	*x = mapWrapper{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_koinos_test_map_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *mapWrapper) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*mapWrapper) ProtoMessage() {}
+
+func (x *mapWrapper) ProtoReflect() protoreflect.Message {
+	mi := &file_koinos_test_map_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use MapWrapper.ProtoReflect.Descriptor instead.
+func (*mapWrapper) Descriptor() ([]byte, []int) {
+	return file_koinos_test_map_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *mapWrapper) GetNestedMap() *testMap {
+	if x != nil {
+		return x.NestedMap
+	}
+	return nil
+}
+
 var File_koinos_test_map_proto protoreflect.FileDescriptor
 
 var file_koinos_test_map_proto_rawDesc = []byte{
@@ -80,7 +127,11 @@ var file_koinos_test_map_proto_rawDesc = []byte{
 	0x54, 0x68, 0x65, 0x4d, 0x61, 0x70, 0x45, 0x6e, 0x74, 0x72, 0x79, 0x12, 0x10, 0x0a, 0x03, 0x6b,
 	0x65, 0x79, 0x18, 0x01, 0x20, 0x01, 0x28, 0x04, 0x52, 0x03, 0x6b, 0x65, 0x79, 0x12, 0x14, 0x0a,
 	0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x05, 0x76, 0x61,
-	0x6c, 0x75, 0x65, 0x3a, 0x02, 0x38, 0x01, 0x42, 0x2e, 0x5a, 0x2c, 0x67, 0x69, 0x74, 0x68, 0x75,
+	0x6c, 0x75, 0x65, 0x3a, 0x02, 0x38, 0x01, 0x22, 0x3e, 0x0a, 0x0b, 0x6d, 0x61, 0x70, 0x5f, 0x77,
+	0x72, 0x61, 0x70, 0x70, 0x65, 0x72, 0x12, 0x2f, 0x0a, 0x0a, 0x6e, 0x65, 0x73, 0x74, 0x65, 0x64,
+	0x5f, 0x6d, 0x61, 0x70, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x10, 0x2e, 0x6b, 0x6f, 0x69,
+	0x6e, 0x6f, 0x73, 0x2e, 0x74, 0x65, 0x73, 0x74, 0x5f, 0x6d, 0x61, 0x70, 0x52, 0x09, 0x6e, 0x65,
+	0x73, 0x74, 0x65, 0x64, 0x4d, 0x61, 0x70, 0x42, 0x2e, 0x5a, 0x2c, 0x67, 0x69, 0x74, 0x68, 0x75,
 	0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6b, 0x6f, 0x69, 0x6e, 0x6f, 0x73, 0x2f, 0x6b, 0x6f, 0x69,
 	0x6e, 0x6f, 0x73, 0x2d, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2d, 0x67, 0x6f, 0x6c, 0x61, 0x6e, 0x67,
 	0x2f, 0x6b, 0x6f, 0x69, 0x6e, 0x6f, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
@@ -98,18 +149,20 @@ func file_koinos_test_map_proto_rawDescGZIP() []byte {
 	return file_koinos_test_map_proto_rawDescData
 }
 
-var file_koinos_test_map_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_koinos_test_map_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_koinos_test_map_proto_goTypes = []interface{}{
-	(*testMap)(nil), // 0: koinos.test_map
-	nil,             // 1: koinos.test_map.ImTheMapEntry
+	(*testMap)(nil),    // 0: koinos.test_map
+	(*mapWrapper)(nil), // 1: koinos.map_wrapper
+	nil,                // 2: koinos.test_map.ImTheMapEntry
 }
 var file_koinos_test_map_proto_depIdxs = []int32{
-	1, // 0: koinos.test_map.im_the_map:type_name -> koinos.test_map.ImTheMapEntry
-	1, // [1:1] is the sub-list for method output_type
-	1, // [1:1] is the sub-list for method input_type
-	1, // [1:1] is the sub-list for extension type_name
-	1, // [1:1] is the sub-list for extension extendee
-	0, // [0:1] is the sub-list for field type_name
+	2, // 0: koinos.test_map.im_the_map:type_name -> koinos.test_map.ImTheMapEntry
+	0, // 1: koinos.map_wrapper.nested_map:type_name -> koinos.test_map
+	2, // [2:2] is the sub-list for method output_type
+	2, // [2:2] is the sub-list for method input_type
+	2, // [2:2] is the sub-list for extension type_name
+	2, // [2:2] is the sub-list for extension extendee
+	0, // [0:2] is the sub-list for field type_name
 }
 
 func init() { file_koinos_test_map_proto_init() }
@@ -130,6 +183,18 @@ func file_koinos_test_map_proto_init() {
 				return nil
 			}
 		}
+		file_koinos_test_map_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*mapWrapper); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -137,7 +202,7 @@ func file_koinos_test_map_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_koinos_test_map_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   3,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
