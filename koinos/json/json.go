c703949166dd593c3ffd46064d17b9e32a3efed8
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-10-15 22:47:43 -0700

    Implement byte field overloads

--- koinos/json/json.go
+++ koinos/json/json.go
@@ -6,6 +6,7 @@ import (
 	gojson "encoding/json"
 	"errors"
 
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/types/descriptorpb"
@@ -14,97 +15,108 @@ import (
 	"github.com/koinos/koinos-proto-golang/koinos"
 )
 
-const(
-	KoinosFieldOverrides = protojson.UnmarshalOptions{
-		FieldOverrides: map[pref.Kind]protojson.FieldOverride{
-			pref.BytesKind: protojson.FieldOverride{
-				MarshalField: func(val pref.Value, fd pref.FieldDescriptor, opt protojson.MarshalOptions) ([]byte, error) {
-					var encodedStr string
-					opts := fd.Options()
+var (
+	KoinosFieldOverrides = map[protoreflect.Kind]protojson.FieldOverride{
+		protoreflect.BytesKind: protojson.FieldOverride{
+			MarshalField: func(val protoreflect.Value, fd protoreflect.FieldDescriptor, opt protojson.MarshalOptions) ([]byte, error) {
+				var encodedStr string
+				opts := fd.Options()
 
-					if opts != nil {
-						fieldOpts := opts.(*descriptorpb.FieldOptions)
-						ext := koinos.E_KoinosBytesType.TypeDescriptor()
-						enum := fieldOpts.ProtoReflect().Get(ext).Enum()
+				if opts != nil {
+					fieldOpts := opts.(*descriptorpb.FieldOptions)
+					ext := koinos.E_KoinosBytesType.TypeDescriptor()
+					enum := fieldOpts.ProtoReflect().Get(ext).Enum()
 
-						switch koinos.BytesType(enum) {
-						case koinos.BytesType_BLOCK_ID:
-							fallthrough
-						case koinos.BytesType_TRANSACTION_ID:
-							encodedStr = "0x" + hex.EncodeToString(v.Bytes())
-						case koinos.BytesType_CONTRACT_ID:
-							fallthrough
-						case koinos.BytesType_ADDRESS:
-							encodedStr = base58.Encode(v.Bytes())
-						default:
-							encodedStr = base64.URLEncoding.EncodeToString(v.Bytes())
+					switch koinos.BytesType(enum) {
+					case koinos.BytesType_HEX:
+						fallthrough
+					case koinos.BytesType_BLOCK_ID:
+						fallthrough
+					case koinos.BytesType_TRANSACTION_ID:
+						encodedStr = "0x" + hex.EncodeToString(val.Bytes())
+					case koinos.BytesType_BASE58:
+						fallthrough
+					case koinos.BytesType_CONTRACT_ID:
+						fallthrough
+					case koinos.BytesType_ADDRESS:
+						encodedStr = base58.Encode(val.Bytes())
+					case koinos.BytesType_BASE64:
+						fallthrough
+					default:
+						encodedStr = base64.URLEncoding.EncodeToString(val.Bytes())
+					}
+				} else {
+					encodedStr = base64.URLEncoding.EncodeToString(val.Bytes())
+				}
+
+				return gojson.Marshal(encodedStr)
+			},
+			UnmarshalField: func(b []byte, fd protoreflect.FieldDescriptor, opt protojson.UnmarshalOptions) (protoreflect.Value, error) {
+				val := protoreflect.Value{}
+
+				var decodedBytes []byte
+				var encodedStr string
+				err := gojson.Unmarshal(b, &encodedStr)
+				if err != nil {
+					return val, err
+				}
+
+				opts := fd.Options()
+				if opts != nil {
+					fieldOpts := opts.(*descriptorpb.FieldOptions)
+					ext := koinos.E_KoinosBytesType.TypeDescriptor()
+					enum := fieldOpts.ProtoReflect().Get(ext).Enum()
+
+					switch koinos.BytesType(enum) {
+					case koinos.BytesType_HEX:
+						fallthrough
+					case koinos.BytesType_BLOCK_ID:
+						fallthrough
+					case koinos.BytesType_TRANSACTION_ID:
+						{
+							if len(encodedStr) < 2 || encodedStr[:2] != "0x" {
+								return val, errors.New("hex string not prepended with '0x'")
+							}
+
+							decodedBytes, err = hex.DecodeString(encodedStr[2:])
 						}
-					} else {
-						encodedStr = base64.URLEncoding.EncodeToString(v.Bytes())
-					}
-
-					return gojson.Marshal(encodedStr)
-				},
-				UnmarshalField: func(b []byte, fd pref.FieldDescriptor, opt protojson.UnmarshalOptions) (pref.Value, error) {
-					val := protoreflect.Value{}
-
-					var decodedBytes byte[]
-					var encodedStr string
-					err := gojson.Unmarshal(b, &encodedStr)
-					if err != nil {
-						return val, err
-					}
-
-					opts := fd.Options()
-					if opts != nil {
-						fieldOpts := opts.(*descriptorpb.FieldOptions)
-						ext := koinos.E_KoinosBytesType.TypeDescriptor()
-						enum := fieldOpts.ProtoReflect().Get(ext).Enum()
-
-						switch koinos.BytesType(enum) {
-						case koinos.BytesType_BLOCK_ID:
-							fallthrough
-						case koinos.BytesType_TRANSACTION_ID:
-							{
-								if len(encodedStr) < 2 || encodedStr[:2] != "0x" {
-									return val, errors.New("hex string not prepended with '0x'")
-								}
-
-								decodedBytes, err = hex.DecodeString(encodedStr[2:])
-							}
-						case koinos.BytesType_CONTRACT_ID:
-							fallthrough
-						case koinos.BytesType_ADDRESS:
-							{
-								decodedBytes = base58.Decode(encodedStr)
-								if decodedBytes == nil {
-									err = errors.New("error decoding base58")
-								}
-							}
-						default:
-							{
-								decodedBytes, err = base64.URLEncoding.DecodeString(encodedStr)
+					case koinos.BytesType_BASE58:
+						fallthrough
+					case koinos.BytesType_CONTRACT_ID:
+						fallthrough
+					case koinos.BytesType_ADDRESS:
+						{
+							decodedBytes = base58.Decode(encodedStr)
+							if len(decodedBytes) == 0 && len(encodedStr) != 0 {
+								err = errors.New("error decoding base58")
 							}
 						}
-					} else {
-						decodedBytes, err = base64.URLEncoding.DecodeString(encodedStr)
+					case koinos.BytesType_BASE64:
+						fallthrough
+					default:
+						{
+							decodedBytes, err = base64.URLEncoding.DecodeString(encodedStr)
+						}
 					}
+				} else {
+					decodedBytes, err = base64.URLEncoding.DecodeString(encodedStr)
+				}
 
-					return protoreflect.ValueOfBytes(bytes), err
-				},
+				return protoreflect.ValueOfBytes(decodedBytes), err
 			},
 		},
 	}
 
-	KoinosMarshalOptions = protoreflect.MarshalOptions{
-		Multiline: false,
-		UseProtoNames: true,
-		FieldOverrides: KoinosFieldOverrides
+	KoinosMarshalOptions = protojson.MarshalOptions{
+		Multiline:      false,
+		Indent:         "",
+		UseProtoNames:  true,
+		FieldOverrides: KoinosFieldOverrides,
 	}
 
-	KoinsUnmarshalOptions = protoreflect.UnmarshalOptions{
+	KoinosUnmarshalOptions = protojson.UnmarshalOptions{
 		DiscardUnknown: true,
-		FieldOverrides: KoinosFieldOverrides
+		FieldOverrides: KoinosFieldOverrides,
 	}
 )
 
--- koinos/json/json_test.go
+++ koinos/json/json_test.go
@@ -2,9 +2,108 @@ package json
 
 import (
 	"bytes"
+	"encoding/base64"
+	"encoding/hex"
+	gojson "encoding/json"
 	"fmt"
 	"testing"
 
-	"github.com/koinos/koinos-proto-golang/koinos"
+	"github.com/btcsuite/btcutil/base58"
+	"github.com/iancoleman/orderedmap"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestByteEncodings(t *testing.T) {
+	testData := []byte{0x66, 0x6f, 0x6f, 0x62, 0x61, 0x72}
+	testObj := &BytesSerializationTestObject{}
+	testObj.Base_64 = testData
+	testObj.Base_58 = testData
+	testObj.Hex = testData
+	testObj.BlockId = testData
+	testObj.TransactionId = testData
+	testObj.ContractId = testData
+	testObj.Address = testData
+	testObj.Default = testData
+
+	hexStr := "0x" + hex.EncodeToString(testData)
+	base58Str := base58.Encode(testData)
+	base64Str := base64.URLEncoding.EncodeToString(testData)
+
+	jsonMap := orderedmap.New()
+	jsonMap.Set("base_64", base64Str)
+	jsonMap.Set("base_58", base58Str)
+	jsonMap.Set("hex", hexStr)
+	jsonMap.Set("block_id", hexStr)
+	jsonMap.Set("transaction_id", hexStr)
+	jsonMap.Set("contract_id", base58Str)
+	jsonMap.Set("address", base58Str)
+	jsonMap.Set("default", base64Str)
+
+	expectedBytes, _ := gojson.Marshal(jsonMap)
+
+	jsonBytes, err := Marshal(testObj)
+	assert.NoError(t, err)
+	assert.True(t, string(expectedBytes) == string(jsonBytes))
+
+	fmt.Printf(string(expectedBytes) + "\n")
+	fmt.Printf(string(jsonBytes) + "\n")
+
+	unmarshalledObject := &BytesSerializationTestObject{}
+	err = Unmarshal(jsonBytes, unmarshalledObject)
+	assert.NoError(t, err)
+	assert.True(t, bytes.Equal(testObj.Base_64, unmarshalledObject.Base_64))
+	assert.True(t, bytes.Equal(testObj.Base_58, unmarshalledObject.Base_58))
+	assert.True(t, bytes.Equal(testObj.Hex, unmarshalledObject.Hex))
+	assert.True(t, bytes.Equal(testObj.BlockId, unmarshalledObject.BlockId))
+	assert.True(t, bytes.Equal(testObj.TransactionId, unmarshalledObject.TransactionId))
+	assert.True(t, bytes.Equal(testObj.ContractId, unmarshalledObject.ContractId))
+	assert.True(t, bytes.Equal(testObj.Address, unmarshalledObject.Address))
+
+	// Test bad encodings
+	jsonMap.Set("base_64", hexStr)
+	jsonBytes, _ = gojson.Marshal(jsonMap)
+	err = Unmarshal(jsonBytes, unmarshalledObject)
+	assert.Error(t, err)
+
+	jsonMap.Set("base_64", base64Str)
+	jsonMap.Set("base_58", hexStr)
+	jsonBytes, _ = gojson.Marshal(jsonMap)
+	err = Unmarshal(jsonBytes, unmarshalledObject)
+	assert.Error(t, err)
+
+	jsonMap.Set("base_58", base58Str)
+	jsonMap.Set("hex", base64Str)
+	jsonBytes, _ = gojson.Marshal(jsonMap)
+	err = Unmarshal(jsonBytes, unmarshalledObject)
+	assert.Error(t, err)
+
+	jsonMap.Set("hex", hexStr)
+	jsonMap.Set("block_id", base64Str)
+	jsonBytes, _ = gojson.Marshal(jsonMap)
+	err = Unmarshal(jsonBytes, unmarshalledObject)
+	assert.Error(t, err)
+
+	jsonMap.Set("block_id", hexStr)
+	jsonMap.Set("transaction_id", base64Str)
+	jsonBytes, _ = gojson.Marshal(jsonMap)
+	err = Unmarshal(jsonBytes, unmarshalledObject)
+	assert.Error(t, err)
+
+	jsonMap.Set("transaction_id", hexStr)
+	jsonMap.Set("contract_id", hexStr)
+	jsonBytes, _ = gojson.Marshal(jsonMap)
+	err = Unmarshal(jsonBytes, unmarshalledObject)
+	assert.Error(t, err)
+
+	jsonMap.Set("contract_id", base58Str)
+	jsonMap.Set("address", hexStr)
+	jsonBytes, _ = gojson.Marshal(jsonMap)
+	err = Unmarshal(jsonBytes, unmarshalledObject)
+	assert.Error(t, err)
+
+	jsonMap.Set("address", base58Str)
+	jsonMap.Set("default", hexStr)
+	jsonBytes, _ = gojson.Marshal(jsonMap)
+	err = Unmarshal(jsonBytes, unmarshalledObject)
+	assert.Error(t, err)
+}
