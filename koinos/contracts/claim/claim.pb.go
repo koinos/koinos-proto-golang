f97fcbb9ebd5061c5a0fe0743acb82c5a873e231
Koinos <67607993+koinos-ci@users.noreply.github.com>
2022-08-10 17:50:33 +0000

    Update for koinos-proto commit 5bb7574

--- koinos/contracts/claim/claim.pb.go
+++ koinos/contracts/claim/claim.pb.go
@@ -333,6 +333,100 @@ func (x *GetInfoResult) GetValue() *ClaimInfo {
 	return nil
 }
 
+type CheckClaimArguments struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	EthAddress []byte `protobuf:"bytes,1,opt,name=eth_address,json=ethAddress,proto3" json:"eth_address,omitempty"`
+}
+
+func (x *CheckClaimArguments) Reset() {
+	*x = CheckClaimArguments{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_koinos_contracts_claim_claim_proto_msgTypes[6]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *CheckClaimArguments) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*CheckClaimArguments) ProtoMessage() {}
+
+func (x *CheckClaimArguments) ProtoReflect() protoreflect.Message {
+	mi := &file_koinos_contracts_claim_claim_proto_msgTypes[6]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use CheckClaimArguments.ProtoReflect.Descriptor instead.
+func (*CheckClaimArguments) Descriptor() ([]byte, []int) {
+	return file_koinos_contracts_claim_claim_proto_rawDescGZIP(), []int{6}
+}
+
+func (x *CheckClaimArguments) GetEthAddress() []byte {
+	if x != nil {
+		return x.EthAddress
+	}
+	return nil
+}
+
+type CheckClaimResult struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Value *ClaimStatus `protobuf:"bytes,1,opt,name=value,proto3" json:"value,omitempty"`
+}
+
+func (x *CheckClaimResult) Reset() {
+	*x = CheckClaimResult{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_koinos_contracts_claim_claim_proto_msgTypes[7]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *CheckClaimResult) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*CheckClaimResult) ProtoMessage() {}
+
+func (x *CheckClaimResult) ProtoReflect() protoreflect.Message {
+	mi := &file_koinos_contracts_claim_claim_proto_msgTypes[7]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use CheckClaimResult.ProtoReflect.Descriptor instead.
+func (*CheckClaimResult) Descriptor() ([]byte, []int) {
+	return file_koinos_contracts_claim_claim_proto_rawDescGZIP(), []int{7}
+}
+
+func (x *CheckClaimResult) GetValue() *ClaimStatus {
+	if x != nil {
+		return x.Value
+	}
+	return nil
+}
+
 var File_koinos_contracts_claim_claim_proto protoreflect.FileDescriptor
 
 var file_koinos_contracts_claim_claim_proto_rawDesc = []byte{
@@ -372,11 +466,21 @@ var file_koinos_contracts_claim_claim_proto_rawDesc = []byte{
 	0x0a, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x22, 0x2e,
 	0x6b, 0x6f, 0x69, 0x6e, 0x6f, 0x73, 0x2e, 0x63, 0x6f, 0x6e, 0x74, 0x72, 0x61, 0x63, 0x74, 0x73,
 	0x2e, 0x63, 0x6c, 0x61, 0x69, 0x6d, 0x2e, 0x63, 0x6c, 0x61, 0x69, 0x6d, 0x5f, 0x69, 0x6e, 0x66,
-	0x6f, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x42, 0x3e, 0x5a, 0x3c, 0x67, 0x69, 0x74, 0x68,
-	0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6b, 0x6f, 0x69, 0x6e, 0x6f, 0x73, 0x2f, 0x6b, 0x6f,
-	0x69, 0x6e, 0x6f, 0x73, 0x2d, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2d, 0x67, 0x6f, 0x6c, 0x61, 0x6e,
-	0x67, 0x2f, 0x6b, 0x6f, 0x69, 0x6e, 0x6f, 0x73, 0x2f, 0x63, 0x6f, 0x6e, 0x74, 0x72, 0x61, 0x63,
-	0x74, 0x73, 0x2f, 0x63, 0x6c, 0x61, 0x69, 0x6d, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x6f, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x22, 0x3e, 0x0a, 0x15, 0x63, 0x68, 0x65, 0x63,
+	0x6b, 0x5f, 0x63, 0x6c, 0x61, 0x69, 0x6d, 0x5f, 0x61, 0x72, 0x67, 0x75, 0x6d, 0x65, 0x6e, 0x74,
+	0x73, 0x12, 0x25, 0x0a, 0x0b, 0x65, 0x74, 0x68, 0x5f, 0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73,
+	0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x42, 0x04, 0x80, 0xb5, 0x18, 0x02, 0x52, 0x0a, 0x65, 0x74,
+	0x68, 0x41, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x22, 0x50, 0x0a, 0x12, 0x63, 0x68, 0x65, 0x63,
+	0x6b, 0x5f, 0x63, 0x6c, 0x61, 0x69, 0x6d, 0x5f, 0x72, 0x65, 0x73, 0x75, 0x6c, 0x74, 0x12, 0x3a,
+	0x0a, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x24, 0x2e,
+	0x6b, 0x6f, 0x69, 0x6e, 0x6f, 0x73, 0x2e, 0x63, 0x6f, 0x6e, 0x74, 0x72, 0x61, 0x63, 0x74, 0x73,
+	0x2e, 0x63, 0x6c, 0x61, 0x69, 0x6d, 0x2e, 0x63, 0x6c, 0x61, 0x69, 0x6d, 0x5f, 0x73, 0x74, 0x61,
+	0x74, 0x75, 0x73, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x42, 0x3e, 0x5a, 0x3c, 0x67, 0x69,
+	0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6b, 0x6f, 0x69, 0x6e, 0x6f, 0x73, 0x2f,
+	0x6b, 0x6f, 0x69, 0x6e, 0x6f, 0x73, 0x2d, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2d, 0x67, 0x6f, 0x6c,
+	0x61, 0x6e, 0x67, 0x2f, 0x6b, 0x6f, 0x69, 0x6e, 0x6f, 0x73, 0x2f, 0x63, 0x6f, 0x6e, 0x74, 0x72,
+	0x61, 0x63, 0x74, 0x73, 0x2f, 0x63, 0x6c, 0x61, 0x69, 0x6d, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x33,
 }
 
 var (
@@ -391,22 +495,25 @@ func file_koinos_contracts_claim_claim_proto_rawDescGZIP() []byte {
 	return file_koinos_contracts_claim_claim_proto_rawDescData
 }
 
-var file_koinos_contracts_claim_claim_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
+var file_koinos_contracts_claim_claim_proto_msgTypes = make([]protoimpl.MessageInfo, 8)
 var file_koinos_contracts_claim_claim_proto_goTypes = []interface{}{
-	(*ClaimInfo)(nil),        // 0: koinos.contracts.claim.claim_info
-	(*ClaimStatus)(nil),      // 1: koinos.contracts.claim.claim_status
-	(*ClaimArguments)(nil),   // 2: koinos.contracts.claim.claim_arguments
-	(*ClaimResult)(nil),      // 3: koinos.contracts.claim.claim_result
-	(*GetInfoArguments)(nil), // 4: koinos.contracts.claim.get_info_arguments
-	(*GetInfoResult)(nil),    // 5: koinos.contracts.claim.get_info_result
+	(*ClaimInfo)(nil),           // 0: koinos.contracts.claim.claim_info
+	(*ClaimStatus)(nil),         // 1: koinos.contracts.claim.claim_status
+	(*ClaimArguments)(nil),      // 2: koinos.contracts.claim.claim_arguments
+	(*ClaimResult)(nil),         // 3: koinos.contracts.claim.claim_result
+	(*GetInfoArguments)(nil),    // 4: koinos.contracts.claim.get_info_arguments
+	(*GetInfoResult)(nil),       // 5: koinos.contracts.claim.get_info_result
+	(*CheckClaimArguments)(nil), // 6: koinos.contracts.claim.check_claim_arguments
+	(*CheckClaimResult)(nil),    // 7: koinos.contracts.claim.check_claim_result
 }
 var file_koinos_contracts_claim_claim_proto_depIdxs = []int32{
 	0, // 0: koinos.contracts.claim.get_info_result.value:type_name -> koinos.contracts.claim.claim_info
-	1, // [1:1] is the sub-list for method output_type
-	1, // [1:1] is the sub-list for method input_type
-	1, // [1:1] is the sub-list for extension type_name
-	1, // [1:1] is the sub-list for extension extendee
-	0, // [0:1] is the sub-list for field type_name
+	1, // 1: koinos.contracts.claim.check_claim_result.value:type_name -> koinos.contracts.claim.claim_status
+	2, // [2:2] is the sub-list for method output_type
+	2, // [2:2] is the sub-list for method input_type
+	2, // [2:2] is the sub-list for extension type_name
+	2, // [2:2] is the sub-list for extension extendee
+	0, // [0:2] is the sub-list for field type_name
 }
 
 func init() { file_koinos_contracts_claim_claim_proto_init() }
@@ -487,6 +594,30 @@ func file_koinos_contracts_claim_claim_proto_init() {
 				return nil
 			}
 		}
+		file_koinos_contracts_claim_claim_proto_msgTypes[6].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*CheckClaimArguments); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_koinos_contracts_claim_claim_proto_msgTypes[7].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*CheckClaimResult); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -494,7 +625,7 @@ func file_koinos_contracts_claim_claim_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_koinos_contracts_claim_claim_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   6,
+			NumMessages:   8,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
