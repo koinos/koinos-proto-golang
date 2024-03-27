203ce98650bd117b5b4eec89bea2b708a865e66a
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2024-03-27 14:41:22 -0600

    Cleanup workflows, ci, and .travis.yml

--- ci/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x
-
-go test -v github.com/koinos/koinos-proto-golang/koinos/canonical -coverprofile=./build/canonical.out -coverpkg=./koinos/canonical
-gcov2lcov -infile=./build/canonical.out -outfile=./build/canonical.info
-
-golangci-lint run ./...
